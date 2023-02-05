a8b05ab3e0022de15bf1012f4c012d57fcaa5770
Johnny Sin <johnnysin@Johnnys-Air.hsd1.ca.comcast.net>
2023-02-04 18:47:23 -0800

    need both images to continue

--- app/views.py
+++ app/views.py
@@ -28,42 +28,44 @@ def index():
                 file_upload2 = request.files['file_upload2']
                 filename2 = file_upload1.filename
 
-                # Resize and save the uploaded image
-                uploaded_image1 = Image.open(file_upload1).resize((250,160))
-                uploaded_image1.save(os.path.join(app.config['UPLOADED'], 'image1.png'))
-                uploaded_image2 = Image.open(file_upload2).resize((250,160))
-                uploaded_image2.save(os.path.join(app.config['UPLOADED'], 'image2.png'))
+                if file_upload2 and file_upload1:
+                    # Resize and save the uploaded image
+                    uploaded_image1 = Image.open(file_upload1).resize((250,160))
+                    uploaded_image1.save(os.path.join(app.config['UPLOADED'], 'image1.png'))
+                    uploaded_image2 = Image.open(file_upload2).resize((250,160))
+                    uploaded_image2.save(os.path.join(app.config['UPLOADED'], 'image2.png'))
 
-                # Read uploaded and original image as array
-                uploaded_image1 = cv2.imread(os.path.join(app.config['UPLOADED'], 'image1.png'))
-                uploaded_image2 = cv2.imread(os.path.join(app.config['UPLOADED'], 'image2.png'))
+                    # Read uploaded and original image as array
+                    uploaded_image1 = cv2.imread(os.path.join(app.config['UPLOADED'], 'image1.png'))
+                    uploaded_image2 = cv2.imread(os.path.join(app.config['UPLOADED'], 'image2.png'))
 
-                # Convert image into grayscale
-                image1_gray = cv2.cvtColor(uploaded_image1, cv2.COLOR_BGR2GRAY)
-                image2_gray = cv2.cvtColor(uploaded_image2, cv2.COLOR_BGR2GRAY)
+                    # Convert image into grayscale
+                    image1_gray = cv2.cvtColor(uploaded_image1, cv2.COLOR_BGR2GRAY)
+                    image2_gray = cv2.cvtColor(uploaded_image2, cv2.COLOR_BGR2GRAY)
 
-                # Calculate structural similarity
-                (score, diff) = structural_similarity(image1_gray, image2_gray, full=True)
-                diff = (diff * 255).astype("uint8")
+                    # Calculate structural similarity
+                    (score, diff) = structural_similarity(image1_gray, image2_gray, full=True)
+                    diff = (diff * 255).astype("uint8")
 
-                # Calculate threshold and contours
-                thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-                cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                cnts = imutils.grab_contours(cnts)
-                
-                # Draw contours on image
-                for c in cnts:
-                    (x, y, w, h) = cv2.boundingRect(c)
-                    cv2.rectangle(uploaded_image1, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                    cv2.rectangle(uploaded_image2, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                    # Calculate threshold and contours
+                    thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+                    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    cnts = imutils.grab_contours(cnts)
+                    
+                    # Draw contours on image
+                    for c in cnts:
+                        (x, y, w, h) = cv2.boundingRect(c)
+                        cv2.rectangle(uploaded_image1, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                        cv2.rectangle(uploaded_image2, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-                # Save all output images (if required)
-                cv2.imwrite(os.path.join(app.config['GENERATED'], 'image_1.png'), uploaded_image1)
-                cv2.imwrite(os.path.join(app.config['GENERATED'], 'image_2.png'), uploaded_image2)
-                cv2.imwrite(os.path.join(app.config['GENERATED'], 'image_diff.png'), diff)
-                cv2.imwrite(os.path.join(app.config['GENERATED'], 'image_thresh.png'), thresh)
-                return render_template('index.html',pred=str(round(score*100,2)) + '%' + ' correct')
-       
+                    # Save all output images (if required)
+                    cv2.imwrite(os.path.join(app.config['GENERATED'], 'image_1.png'), uploaded_image1)
+                    cv2.imwrite(os.path.join(app.config['GENERATED'], 'image_2.png'), uploaded_image2)
+                    cv2.imwrite(os.path.join(app.config['GENERATED'], 'image_diff.png'), diff)
+                    cv2.imwrite(os.path.join(app.config['GENERATED'], 'image_thresh.png'), thresh)
+                    return render_template('index.html',pred=str(round(score*100,2)) + '%' + ' correct')
+                else:
+                    return render_template('index.html',pred=str('Please Input Both Images'))
 # Main function
 if __name__ == '__main__':
     app.run(debug=True)
